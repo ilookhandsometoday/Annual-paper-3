dbec11d45dbf45ab624bf318dbcf40286663370e
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-02-18 16:40:52 +0500

    Implement q and r generation q and r can now be generated

--- keygen.py
+++ keygen.py
@@ -1,21 +1,42 @@
 import random
 import sys
+import math
+
+_RNG = random.SystemRandom()
 
 
 class Keygen:
 
     def __init__(self):
-        pass
+        # w is the sequence for the private key
+        # I'm using 128 elements because that's gonna be the size of the chunks
+        # that the original message is going to be split into for encryption
+        self.w = Keygen.generate_si_sequence(128)
+        self.q = Keygen._find_q(self.w)
+        self.r = Keygen._find_r(self.q)
 
     @staticmethod
-    def generate_si_sequence():
-        """generates a superincreasing sequence of length 128.
-        Number 128 is chosen to ensure security by virtue of making it harder to guess the key and also by making a birthday
-        attack practically impossible"""
-        sysrand = random.SystemRandom()
+    def generate_si_sequence(n):
+        """Generates a superincreasing sequence of length n."""
         sequence = []
         s_sum = 0
-        for i in range(128):
-            sequence.append(sysrand.randint(1, sys.maxsize) + s_sum)
+        for i in range(n):
+            sequence.append(_RNG.randint(1, sys.maxsize) + s_sum)
             s_sum = sum(sequence)
         return sequence
+
+    @staticmethod
+    def _find_r(q):
+        coprime = False
+        r = 0
+        while not coprime:
+            r = _RNG.randint(2, q-1)
+            if math.gcd(q, r) == 1:
+                coprime = True
+        return r
+
+    @staticmethod
+    def _find_q(w):
+        w_sum = sum(w)
+        q = _RNG.randint(w_sum + 1, w_sum + sys.maxsize)
+        return q
--- tests/test_keygen.py
+++ tests/test_keygen.py
@@ -1,11 +1,24 @@
 import unittest
 from keygen import Keygen
+import math
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.kg = Keygen()
+
+    def test_find_q(self):
+        print(self.kg.q)
+        print(sum(self.kg.w))
+        self.assertTrue(self.kg.q > sum(self.kg.w))
+
+    def test_find_r(self):
+        print(self.kg.q)
+        print(self.kg.r)
+        self.assertTrue(math.gcd(self.kg.q, self.kg.r) == 1 and self.kg.q > self.kg.r > 1)
 
     def test_generate_si_sequence(self):
-        seq = self.keygen.generate_si_sequence()
+        seq = Keygen.generate_si_sequence(128)  # 128 because that's the size of chunks
         seq_sum = 0
         test = True
         for element in seq:
@@ -14,10 +27,8 @@ class MyTestCase(unittest.TestCase):
                 break
             seq_sum += element
         print(seq)
-        self.assertTrue(test, seq)
-
-    def setUp(self):
-        self.keygen = Keygen()
+        print(sum(seq))
+        self.assertTrue(test)
 
 
 if __name__ == '__main__':
