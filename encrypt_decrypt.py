8f57dea86aeb55ad2b94d4cdc2ddb5e24fcb42e4
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-01 20:32:11 +0500

    Implement turning string into binary

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,7 +1,15 @@
 import keygen
 
 def encrypt(text, keygen):
+
     pass
 
 def decrypt(text, keygen):
     pass
+
+def _to_binary(text):
+    as_bytes = bytearray(text, "utf-8")
+    binary_values = []
+    for byte in as_bytes:
+        binary_values.append(bin(byte))
+    return binary_values
