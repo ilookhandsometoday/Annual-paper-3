10ffafbd5e053d033ebcdd5e40b64bc7e136c805
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-01 19:59:58 +0500

    Write unit tests for key generation

--- tests/test_keygen.py
+++ tests/test_keygen.py
@@ -5,6 +5,22 @@ import math
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.kg = Keygen()
+
+    def test_sequence_generation(self):
+        sum = 0
+        test = True
+        for element in self.kg.seq:
+            if element < sum:
+                test = False
+                break
+            sum += element
+        self.assertTrue(test)
+
+    def test_modulo_multiplier(self):
+        self.assertTrue(math.gcd(self.kg.modulus, self.kg.multiplier) == 1)
+
 
 
 if __name__ == '__main__':
