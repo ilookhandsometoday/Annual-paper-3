c2a66a099eef7475a1465e9855ee2f2b11e56fb1
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-04 19:27:16 +0500

    Rewrite _to_binary function using map()

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,16 +1,14 @@
-import keygen
-
 def encrypt(text, keygen):
     text_as_binary = _to_binary(text)
+    for char_as_bits in text_as_binary:
+        pass
     pass
 
 def decrypt(text, keygen):
     pass
 
 def _to_binary(text):
+    """Returns a list of characters of the original text in binary form using UTF-8 encoding"""
     as_bytes = bytearray(text, "utf-8")
-    binary_values = []
-    for byte in as_bytes:
-        binary_values.append(bin(byte)[2:])
-
+    binary_values = list(map(lambda byte: bin(byte)[2:], as_bytes))
     return binary_values
