1707e591a1cad2884b38dbe90506111688f0902e
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-12 20:13:23 +0500

    Rework _to_binary() Rework _to_binary to return a list of characters in 8 bit form with all 8 bits present (even the leading zeros) since bin() removes leading zeros from a binary representation of a byte

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,7 +1,7 @@
 def encrypt(text, keygen):
     text_as_binary = _to_binary(text)
     for char_as_bits in text_as_binary:
-        pass
+
     pass
 
 def decrypt(text, keygen):
@@ -11,4 +11,11 @@ def _to_binary(text):
     """Returns a list of characters of the original text in binary form using UTF-8 encoding"""
     as_bytes = bytearray(text, "utf-8")
     binary_values = list(map(lambda byte: bin(byte)[2:], as_bytes))
-    return binary_values
+    #This is done to make it easier to separate the decrypted message into 8 bit chunks, since bin()
+    #removes leading zeros from a binary representation of a byte
+    binary_values_uniform = []
+    for char_as_bits in binary_values:
+        missing_zeroes = 8 - len(char_as_bits)
+        new_char_as_bits = missing_zeroes + char_as_bits
+        binary_values_uniform.append(new_char_as_bits)
+    return binary_values_uniform
