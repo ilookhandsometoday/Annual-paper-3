46105415d9e18de9d070031ebb00115840d6df45
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-13 00:04:10 +0500

    Add a unit test for _to_binary Add a unit test for _to_binary to check if it's adding zeros to the beginning of the bit sequence correctly

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,8 +1,7 @@
 def encrypt(text, keygen):
     text_as_binary = _to_binary(text)
     for char_as_bits in text_as_binary:
-
-    pass
+        pass
 
 def decrypt(text, keygen):
     pass
@@ -16,6 +15,6 @@ def _to_binary(text):
     binary_values_uniform = []
     for char_as_bits in binary_values:
         missing_zeroes = 8 - len(char_as_bits)
-        new_char_as_bits = missing_zeroes + char_as_bits
+        new_char_as_bits = missing_zeroes*"0" + char_as_bits
         binary_values_uniform.append(new_char_as_bits)
     return binary_values_uniform
--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -1,9 +1,16 @@
 import unittest
-
+import encrypt_decrypt
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test__to_binary(self):
+        as_binary = encrypt_decrypt._to_binary(".бb")
+        test = True
+        for binary in as_binary:
+            if len(binary) != 8:
+                test = False
+                break
+        print(as_binary)
+        self.assertTrue(test)
 
 
 if __name__ == '__main__':
