d7d97d05e084d3f24af0a0f4a2bec4fdb2048601
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-14 18:40:12 +0500

    Add unit test for _to_binary Add unit test for _to_binary to see if it works as intended (i.e returns correct bit sequences)

--- tests/test_encrypt_decrypt.py
+++ tests/test_encrypt_decrypt.py
@@ -2,7 +2,7 @@ import unittest
 import encrypt_decrypt
 
 class MyTestCase(unittest.TestCase):
-    def test__to_binary(self):
+    def test__to_binary_leading_zeros(self):
         as_binary = encrypt_decrypt._to_binary(".бb")
         test = True
         for binary in as_binary:
@@ -11,6 +11,10 @@ class MyTestCase(unittest.TestCase):
                 break
         print(as_binary)
         self.assertTrue(test)
+
+    def test__to_binary(self):
+        as_binary = encrypt_decrypt._to_binary("cd")
+        self.assertEqual(as_binary, ["01100011", "01100100"])
 
 
 if __name__ == '__main__':
