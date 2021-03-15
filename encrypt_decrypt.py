1aa6f4d44d0e6be90bb76ea3640c4166d2d5c00e
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-15 12:36:29 +0500

    Refactor encrypt() and start working on decrypt()

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,17 +1,22 @@
 def encrypt(text, keygen):
+    """Ecnrypts text by using the given Merkle-Hellman key generator.
+    Returns a list of encrypted 100-bit(or less) chunks of the original message"""
     text_as_binary = "".join(_to_binary(text))
-    hundred_bit_messages = []
+    encrypted_chunks = []
     # message is split into chunks of 100 bits as per recommendations from Merkle and Hellman
-    for hundred_bits in _chunk_text(text_as_binary):
+    for chunk in _chunk_text(text_as_binary, 100):
         # a variable for the final sum
-        encrypted_bits = 0
-        for bit, open_key_element in zip(hundred_bits, keygen.open_key):
-            encrypted_bits += int(bit) * open_key_element
-        hundred_bit_messages.append(encrypted_bits)
-    return hundred_bit_messages
+        encrypted_chunk = 0
+        for bit, open_key_element in zip(chunk, keygen.open_key):
+            encrypted_chunk += int(bit) * open_key_element
+        encrypted_chunks.append(encrypted_chunk)
+    return encrypted_chunks
 
-def decrypt(text, keygen):
-    pass
+
+def decrypt(encrypted_chunks, keygen):
+    for e_chunk in encrypted_chunks:
+        pass
+
 
 def _to_binary(text):
     """Returns a list of characters of the original text in binary form using UTF-8 encoding"""
@@ -22,7 +27,8 @@ def _to_binary(text):
     binary_values_uniform = [(8 - len(char_as_bits))*"0" + char_as_bits for char_as_bits in binary_values]
     return binary_values_uniform
 
+
 def _chunk_text(text, length):
     """Separates text into chunks of given length"""
-    return (text[0+i:length+i] for i in range(0, len(text), 100))
+    return (text[0+i:length+i] for i in range(0, len(text), length))
 
