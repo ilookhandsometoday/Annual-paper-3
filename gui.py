93b857bd84d860230b43aca21debfd15b9c23fcf
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-19 22:14:25 +0500

    Add basic GUI structure

--- gui.py
+++ gui.py
@@ -1,7 +1,19 @@
 from tkinter import *
 from tkinter.ttk import *
 
-window = Tk()
-window.title("Шифрователь-5000")
-window.mainloop()
+
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+
+    def create_widgets(self):
+        pass
+
+
+root = Tk()
+root.title("Шифрователь-5000")
+app = Application(root)
+app.mainloop()
 
