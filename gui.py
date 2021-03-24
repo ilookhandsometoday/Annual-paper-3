9e2f1266d6b20f341a25c6148eb0c2fe6c552b11
ilookhandsometoday <nikitadenisovvalerevich@gmail.com>
2021-03-24 14:11:50 +0500

    Implement minor changes to gui and start working on tab2 logic

--- gui.py
+++ gui.py
@@ -42,6 +42,8 @@ class Application(Frame):
         self.tab2.encrypt_frame.to_encrypt_text.bind("<KeyRelease>", Application._on_to_encrypt_key_released)
         self.tab2.decrypt_frame.to_decrypt_text.bind("<KeyRelease>", Application._on_to_decrypt_key_released)
 
+        self.tab2.encrypt_frame.encrypt_button.configure(command=self._on_encrypt_button_tab2)
+
         self.tab_control.add(self.tab1, text="Ключи, сгенерированные автоматически")
         self.tab_control.add(self.tab2, text="Пользовательские ключи")
         self.tab_control.pack()
@@ -98,6 +100,18 @@ class Application(Frame):
         else:
             _insert_to_disabled_text(self.tab1.decrypt_frame.decrypted_text, text)
 
+    def _on_encrypt_button_tab2(self):
+        open_key_entry = self.tab2.open_key_text.get().rstrip('\n')
+        if open_key_entry:
+
+            open_key = ast.literal_eval(open_key_entry)
+            if isinstance(open_key, list) and all(isinstance(element, int) for element in open_key):
+                
+
+
+        else:
+            mb.showerror("Ошибка", "Открытый ключ не задан!")
+
 
 class Subframe(Frame):
     """Abstract class created to reuse frame creation code. Should not be used by itself"""
@@ -149,6 +163,12 @@ class UserKeysFrame(Subframe):
     """Layout for the user keys tab"""
 
     def _create_widgets(self):
+        self.warning_label = Label(self, text="Внимание! Ключи для этой вкладки настоятельно "
+                                              "рекомендуется копировать из первой вкладки "
+                                              "копии этого приложения."
+                                              "Формат ключей аналогичен формату ключей на первой вкладке")
+        self.warning_label.pack()
+
         self.open_key_label = Label(self, text="Открытый ключ")
         self.open_key_text = Entry(self)
         self.open_key_label.pack()
